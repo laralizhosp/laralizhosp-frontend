2dc9622e40718c70f9ebc484af7cc702f3f6f229
codemoises <88902469+codemoises@users.noreply.github.com>
2025-02-14 13:44:27 -0300

    arquivos que não preciso

--- src/components/MainContainer/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as Styled from "./styles";
-
-export type MainContainerProps = {
-  children: React.ReactNode;
-};
-export default function MainContainer({ children }: MainContainerProps) {
-  return <Styled.Container>{children}</Styled.Container>;
-}
